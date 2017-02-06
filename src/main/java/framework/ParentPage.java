52525ad532cfebaa857f9445b344f7da4e1bf5c4
RomaOnyshkiv <romaonyshkiv@gmail.com>
2017-02-06 20:01:55 +0200

    initial commit with base structure

---
+++ src/main/java/framework/ParentPage.java
@@ -0,0 +1,7 @@
+package framework;
+
+/**
+ * Created by roma on 2/6/17.
+ */
+public class ParentPage {
+}
---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,7 @@
+package pages;
+
+/**
+ * Created by roma on 2/6/17.
+ */
+public class HomePage {
+}
